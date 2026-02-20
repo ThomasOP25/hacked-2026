edd8083fefa4313db159dd76949c068984fea8cb
jackyy37 <jiale22@ualberta.ca>
2026-02-20 14:20:45 -0700

    Starting pieces

---
+++ chess_pieces.py
@@ -0,0 +1,20 @@
+def main():
+    print("Hello World")
+
+class Rook:
+    pass
+
+class Pawn:
+    pass
+
+class Bishop:
+    pass
+
+class Knight:
+    pass
+
+class King:
+    pass
+
+class Queen:
+    pass
