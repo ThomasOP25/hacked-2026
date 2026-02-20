dc9cba4acf5cb1eed02edbde1ba7e168c290f189
jackyy37 <jiale22@ualberta.ca>
2026-02-20 14:31:42 -0700

    pieces

---
+++ chess_pieces.py
@@ -0,0 +1,21 @@
+def main():
+    print("Hello World")
+
+class Rook:
+    pass
+
+class Bishop:
+    pass
+
+class Pawn:
+    pass
+
+class Knight:
+    pass
+
+class King:
+    pass
+
+class Queen:
+    pass
+
