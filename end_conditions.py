4d0cee278fab259ba9c60abdf10b190fc219a518
jackyy37 <jiale22@ualberta.ca>
2026-02-20 22:26:53 -0700

    Finished diag conditions

--- end_conditions.py
+++ end_conditions.py
@@ -2,48 +2,64 @@ def check_diags(board, cords):
     #Pass in board object and cords list
     safe = True
     diagFU, diagRU, diagFD, diagRD = [x,y]
-    pieces_found = []
     king = board[x][y]
-
+    
+    pieces_found = []
+    
+    #proximal_pk indicates whether a pawn or king is within diag kill range
+    proximal_pieceFU, proximal_pieceRU, proximal_pieceFD, proximal_pieceRD = 0
+    
     #Forward Up diag.
-    while board[diagFU[0]][diagFU[1]] == king or board[diagFU[0]][diagFU[1]] != "0" or diagRU[0] <= 8 or diagRU[1] <= 8:
+    while board[diagFU[0]][diagFU[1]] == king or board[diagFU[0]][diagFU[1]] == "0" or diagRU[0] <= 8 or diagRU[1] <= 8:
         diagFU[0] += 1
         diagFU[1] -= 1
+        proximal_pieceFU += 1
+        
                     
     if board[diagFU[0]][diagFU[1]] != "0":
         pieces_found.append(board[diagFU[0]][diagFU[1]])
+    else:
+        pieces_found.append(0)
+    
                         
     #Reverse Up diag.
-    while board[diagRU[0]][diagRU[1]] == king or board[diagRU[0]][diagRU[1]] != "0" or diagRU[0] <= 8 or diagRU[1] <= 8:
+    while board[diagRU[0]][diagRU[1]] == king or board[diagRU[0]][diagRU[1]] == "0" or diagRU[0] <= 8 or diagRU[1] <= 8:
         diagRU[0] -= 1
         diagRU[1] -= 1
+        proximal_pieceRU += 1
                     
     if board[diagRU[0]][diagRU[1]] != "0":
         pieces_found.append(board[diagRU[0]][diagRU[1]])                
-                    
+    else:
+        pieces_found.append(0)
+        
     #Forward Down diag.
-    while board[diagFD[0]][diagFD[1]] == king or board[diagFD[0]][diagFD[1]] != "0" or diagFD[0] <= 8 or diagFD[1] <= 8:
+    while board[diagFD[0]][diagFD[1]] == king or board[diagFD[0]][diagFD[1]] == "0" or diagFD[0] <= 8 or diagFD[1] <= 8:
         diagFD[0] += 1
         diagFD[1] += 1
+        proximal_pieceFD += 1
                 
     if board[diagFD[0]][diagFD[1]] != "0":
         pieces_found.append(board[diagFD[0]][diagFD[1]])                
-                    
+    else:
+        pieces_found.append(0)
+        
     #Reverse Down diag.
-    while board[diagRD[0]][diagRD[1]] == king or board[diagRD[0]][diagRD[1]] != "0" or diagRD[0] <= 8 or diagRD[1] <= 8:
+    while board[diagRD[0]][diagRD[1]] == king or board[diagRD[0]][diagRD[1]] == "0" or diagRD[0] <= 8 or diagRD[1] <= 8:
         diagRD[0] -= 1
         diagRD[1] += 1
+        proximal_pieceRD += 1
                 
     if board[diagRD[0]][diagRD[1]] != "0":
         pieces_found.append(board[diagRD[0]][diagRD[1]])                 
-                    
-    for piece in pieces_found:
-        if piece == "bishop" and piece.color != king.color:
-            safe = False
+    else:
+        pieces_found.append(0)
         
-        if piece == "pawn" and piece.color != king.color: #Need to add pawn possible kill for enemies
-            safe = False
-    #Check each searched diag.
+    #[FU, RU, FD, RD]                
+    if ((proximal_pieceFU == 1 and pieces_found[0] == "pawn" or pieces_found[0] == "king" and piece.color != king.color) or (proximal_pieceRU == 1 and pieces_found[1] == "pawn" or pieces_found[1] == "king" and piece.color != king.color)) or  ((proximal_pieceFD == 1 and pieces_found[2] == "king" and piece.color != king.color) or (proximal_pieceRD == 1 and pieces_found[2] == "king" and piece.color != king.color)) or (piece == "bishop" and piece.color != king.color) or (piece == "queen" and piece.color != king.color):
+        safe = False
+    
+    #Check after searching possible diags.
     return safe
 
 def check_straights(cord):
