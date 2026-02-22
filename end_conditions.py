5bd907d116e088a24a86131635c54871acf48385
jackyy37 <jiale22@ualberta.ca>
2026-02-21 18:22:32 -0700

    Minor changes to chackmate condition

--- end_conditions.py
+++ end_conditions.py
@@ -9,24 +9,36 @@ def king_check(king, board, pieces_arr):
     return False
     
 def end_condition(turn, board, pieces_arr):
-    end = False
     king = None
+    
+    #Find the current king piece we are looking at
     for piece in pieces_arr:
         if piece.color == turn and piece.__class__.__name__ == "King":
             king = piece
-    
-    unsafe = 0    
-    king_moves = king.get_valid_moves(board, pieces_arr)
-    max_unsafe = len(king_moves)
-    
-    for piece in pieces_arr:
-        if piece.color != turn:
-            for kmove in king_moves:
-                if kmove in piece.get_valid_moves(board, pieces_arr):
-                    unsafe += 1
-    
-    #checkmate                    
-    if unsafe == max_unsafe:
-        end = True
-    
+        
+    end = False
+    if king_check(king, board, pieces_arr):
+        
+        #get all valid moves for king piece
+        unsafe = 0    
+        king_moves = king.get_valid_moves(board, pieces_arr)
+        max_unsafe = len(king_moves)
+        
+        #Cycle through all king piece moves, and see if thery match any enemy piece move
+        for kmove in king_moves:
+            for piece in pieces_arr:
+                if piece.color != king.color:
+                    for move in piece.get_valid_moves(board, pieces_arr):
+                        if move == kmove:
+                            unsafe += 1
+                
+                else:
+                    for move in piece.get_valid_moves(board, pieces_arr):
+                        if move == kmove:
+                            unsafe -= 1
+        
+        #checkmate                    
+        if unsafe == max_unsafe:
+            end = True
+        
     return end
